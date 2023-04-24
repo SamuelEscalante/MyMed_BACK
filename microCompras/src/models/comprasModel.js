e71c7eb1f747788099e2859f7ae7852e4adad36a
saalesgu <saalesgu@gmail.com>
2023-04-23 21:28:51 -0500

    ...

--- microCompras/src/models/comprasModel.js
+++ microCompras/src/models/comprasModel.js
@@ -14,7 +14,7 @@ async function crearCompra(compra) {
 }
 async function crearDetalleCompra(compras) {  
     console.log(compras);
-    const result = await connection.query(`INSERT INTO medicamentos_por_usuarios (id, usuario, medicamento_nombre, cantidad, precio_total) VALUES ${compras})`, []);
+    const result = await connection.query(`INSERT INTO medicamentos_por_usuarios (id, usuario, medicamento_nombre, cantidad, precio_total) VALUES ${compras}`, []);
     return result;
 }
 async function traerCompra(id) {
